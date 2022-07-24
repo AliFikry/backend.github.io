dd65f05f4c6d79d4ff62d3e216807d19516dce52
AliFikry <58614974+AliFikry@users.noreply.github.com>
2022-07-24 16:56:40 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -86,64 +86,57 @@
 // firebase.database().ref("matchById/").remove()
 
 
-time.forEach(e => {
-    // https://apiv3.apifootball.com/?action=get_events&from=2021-07-12&to=2021-07-12&league_id=1&APIkey=e3554144c5e71116340837a3ffd789e5cf103fe8da6e50dcc0ba99b90bef5db3
-    fetch(`https://apiv3.apifootball.com/?action=get_events&from=${e}&to=${e}&APIkey=${apiKey}`)
-        .then(response => response.json())
-        .then(result => {
-            console.log(e);
-            console.log(result);
-            for (let i = 0; i < result.length; i++) {
-                firebase.database().ref("matchById/" + result[i]["match_id"] + "/data/").set({
-                    match_id: result[i]["match_id"],
-                    country_name: result[i]["country_name"],
-                    league_name: result[i]["league_name"],
-                    league_id: result[i]["league_id"],
-                    match_time: result[i]["match_time"],
-                    match_hometeam_id: result[i]["match_hometeam_id"],
-                    match_hometeam_name: result[i]["match_hometeam_name"],
-                    match_hometeam_score: result[i]["match_hometeam_score"],
-                    match_awayteam_name: result[i]["match_awayteam_name"],
-                    match_awayteam_id: result[i]["match_awayteam_id"],
-                    match_awayteam_score: result[i]["match_awayteam_score"],
-                    match_hometeam_ft_score: result[i]["match_hometeam_ft_score"],
-                    match_awayteam_ft_score: result[i]["match_awayteam_ft_score"],
-                    match_hometeam_extra_score: result[i]["match_hometeam_extra_score"],
-                    match_awayteam_extra_score: result[i]["match_awayteam_extra_score"],
-                    match_hometeam_penalty_score: result[i]["match_hometeam_penalty_score"],
-                    match_awayteam_penalty_score: result[i]["match_awayteam_penalty_score"],
-                    match_hometeam_system: result[i]["match_hometeam_system"],
-                    match_awayteam_system: result[i]["match_awayteam_system"],
-                    match_live: result[i]["match_live"],
-                    match_date: result[i]["match_date"],
-                    match_stadium: result[i]["match_stadium"],
-                    match_referee: result[i]["match_referee"],
-                    team_home_badge: result[i]["team_home_badge"],
-                    team_away_badge: result[i]["team_away_badge"],
-                    goalscorer: result[i]["goalscorer"],
-                    cards: result[i]["cards"],
-                    substitutions: result[i]["substitutions"],
-                    lineup: result[i]["lineup"],
-                    statistics: result[i]["statistics"],
-                })
-                fetch(`https://apiv3.apifootball.com/?action=get_predictions&match_id=${result[i]["match_id"]}&APIkey=${apiKey}`)
-                    .then(response => response.json())
-                    .then(pre => {
-                        if (pre[0]["error"] != 404) {
-                            firebase.database().ref("matchById/" + result[i]["match_id"] + "/data/pre/").set({
-                                prob_HW: pre[0]["prob_HW"],
-                                prob_D: pre[0]["prob_D"],
-                                prob_AW: pre[0]["prob_AW"]
-                            })
-                        }
-                    })
+// time.forEach(e => {
+//     // https://apiv3.apifootball.com/?action=get_events&from=2021-07-12&to=2021-07-12&league_id=1&APIkey=e3554144c5e71116340837a3ffd789e5cf103fe8da6e50dcc0ba99b90bef5db3
+//     fetch(`https://apiv3.apifootball.com/?action=get_events&from=${e}&to=${e}&APIkey=${apiKey}`)
+//         .then(response => response.json())
+//         .then(result => {
+//             console.log(e);
+//             console.log(result);
+//             for (let i = 0; i < result.length; i++) {
+//                 firebase.database().ref("matchById/" + result[i]["match_id"] + "/data/").set({
+//                     match_id: result[i]["match_id"],
+//                     country_name: result[i]["country_name"],
+//                     league_name: result[i]["league_name"],
+//                     league_id: result[i]["league_id"],
+//                     match_time: result[i]["match_time"],
+//                     match_hometeam_id: result[i]["match_hometeam_id"],
+//                     match_hometeam_name: result[i]["match_hometeam_name"],
+//                     match_hometeam_score: result[i]["match_hometeam_score"],
+//                     match_awayteam_name: result[i]["match_awayteam_name"],
+//                     match_awayteam_id: result[i]["match_awayteam_id"],
+//                     match_awayteam_score: result[i]["match_awayteam_score"],
+//                     match_hometeam_ft_score: result[i]["match_hometeam_ft_score"],
+//                     match_awayteam_ft_score: result[i]["match_awayteam_ft_score"],
+//                     match_hometeam_extra_score: result[i]["match_hometeam_extra_score"],
+//                     match_awayteam_extra_score: result[i]["match_awayteam_extra_score"],
+//                     match_hometeam_penalty_score: result[i]["match_hometeam_penalty_score"],
+//                     match_awayteam_penalty_score: result[i]["match_awayteam_penalty_score"],
+//                     match_hometeam_system: result[i]["match_hometeam_system"],
+//                     match_awayteam_system: result[i]["match_awayteam_system"],
+//                     match_live: result[i]["match_live"],
+//                     match_date: result[i]["match_date"],
+//                     match_stadium: result[i]["match_stadium"],
+//                     match_referee: result[i]["match_referee"],
+//                     team_home_badge: result[i]["team_home_badge"],
+//                     team_away_badge: result[i]["team_away_badge"],
+//                     goalscorer: result[i]["goalscorer"],
+//                     cards: result[i]["cards"],
+//                     substitutions: result[i]["substitutions"],
+//                     lineup: result[i]["lineup"],
+//                     statistics: result[i]["statistics"],
+//                 })
+              
 
-            }
-        })
-});
+//             }
+//         })
+// });
 
+fetch(`https://apiv3.apifootball.com/?action=get_events&from=${moment().subtract(120, "days").format("yyyy[-]MM[-]DD").toString()}&to=${moment().add(90, "days").format("yyyy[-]MM[-]DD").toString()}&APIkey=${apiKey}`)
 
+.then(response=>response.json())
+.then(result =>{
+console.log(result);
+})
 
-
-
-//firebase.database().ref("matchById/").remove()
+firebase.database().ref("matchById/").remove()
